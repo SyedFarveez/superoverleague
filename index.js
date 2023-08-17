007e714f07d1c9f8bb157d0599739b75ad5a5225
syed farveez <syedfarveez11@gmail.com>
2023-08-17 08:06:15 +0000

    my first commit

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
-const TeamCard = (props) => {
-  const { imageUrl, name } = props;
-  return (
-    <div className="team-card">
-      <img src={imageUrl} alt={name} className="team-logo" />
-      <p className="team-name">{name}</p>
-    </div>
-  );
-};
-
-const SuperOverLeague = () => {
-  return (
-    <div className="super-over-league">
-      <h1 className="league-heading">Super Over League</h1>
-      <div className="teams-container">
-        <TeamCard
-          imageUrl="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
-          name="RCB"
-        />
-        <TeamCard
-          imageUrl="https://assets.ccbp.in/frontend/react-js/csk-img.png"
-          name="CSK"
-        />
-      </div>
-      {/* JSX syntax for HTML main heading element */}
-      <h2 className="sub-heading">Welcome to the Exciting Matches!</h2>
-      {/* JSX syntax for HTML image element */}
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <div>
+      <h1 className="heading">Super Over League</h1>
       <img
-        src="https://assets.ccbp.in/frontend/react-js/super-over-image.png"
-        alt="Super Over League"
-        className="super-over-image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image"
       />
     </div>
-  );
-};
+  </div>
+);
 
-ReactDOM.render(<SuperOverLeague />, document.getElementById("root"));
+ReactDOM.render(element, document.getElementById("root"));
+s;
